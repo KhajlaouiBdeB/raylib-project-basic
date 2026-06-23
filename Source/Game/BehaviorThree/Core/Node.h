f1c501f5c591ea99a9f6cf9e9f1eaf005b5a557d
Mario Ynurreta <ynurretam@gmail.com>
2026-06-23 15:42:56 -0400

    [Code] Creation Node Class

--- Source/Game/BehaviorThree/Core/Node.h
+++ Source/Game/BehaviorThree/Core/Node.h
@@ -1,5 +1,27 @@
 #pragma once
-class Node
+
+namespace BehaviourTree
 {
-};
+	enum class NodeState
+	{
+		RUNNING,
+		SUCCESS,
+		FAILURE
+	};
+
+
+
+	class Node
+	{	
+	public:
+		virtual ~Node() = default;
+		virtual NodeState tick() = 0;
+
+	private:
+		Node() = default;
+
+	};
+}
+
+
 
